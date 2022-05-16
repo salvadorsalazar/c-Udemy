c70904f2248b3de4d73553f98c3a975d988d1846
salvador salazar <salvador.g.salazar79@gmail.com>
2022-05-15 23:49:27 -0500

    hello world

--- c#/Program.cs
+++ c#/Program.cs
@@ -10,7 +10,7 @@ namespace CSharpTutorials
     {
         static void Main(string[] args)
         {
-            string message = "Hello World!!";
+            string message = "Hello World!! testing in intellij idea";
 
             Console.WriteLine(message);
         }
